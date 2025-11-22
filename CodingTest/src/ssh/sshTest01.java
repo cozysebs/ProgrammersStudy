3792f8f2d4adbcc319e7bba7cb8a5a167f56c298
sony-soohyun <alvlrhrl@naver.com>
2025-11-22 19:18:54 +0900

    손수현2

--- CodingTest/src/ssh/sshTest01.java
+++ CodingTest/src/ssh/sshTest01.java
@@ -6,35 +6,36 @@ public class sshTest01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] array = {1,5,2,6,3,7,4};
-        int[][] commands = {{2,5,3},{4,4,1}, {1,7,3}};
-        int[] box = {};
-        int[] answer = new int[commands.length];
-
-
-        for (int i=0; i<commands.length; i++) {
-            box = Arrays.copyOfRange(array, commands[i][0]-1,
-                    commands[i][1]);    // {5,2,6,3}
-            
-            int temp;
-            int num1 = 0;
-            while(num1<=box.length) {
-            	for(int j=1;j<box.length;j++){
-                    if(box[j-1]>box[j]) {
-                        temp = box[j-1];
-                        box[j-1] = box[j];
-                        box[j] = temp;
-                    }
-            	}
-            	num1++;
-            }
-            
-            answer[i] = box[commands[i][2]-1];
-            for (int a : box) {
-                System.out.print(a+" ");
-            }
-            System.out.println(" ");
-        }
+		// 코딩테스트 > 알고리즘 고득점 Kit > 정렬 > K번째수
+				int[] array = {1, 5, 2, 6, 3, 7, 4};
+				int[][] commands = {{2,5,3},{4,4,1},{1,7,3}};
+				
+				int[] answer = new int[commands.length];
+				int[] temp = {};
+//				int[] arraycopy = Arrays.copyOfRange(array, 2, 5);
+//				
+//				answer = arraycopy;
+//				Arrays.sort(answer);
+				
+				for(int i=0; i<commands.length; i++) {
+					temp = commands[i];
+					System.out.println(Arrays.toString(temp));
+					
+					int[] arraycopy = Arrays.copyOfRange(array, temp[0]-1, temp[1]);
+					Arrays.sort(arraycopy);
+					System.out.print("카피카피");
+					System.out.println(Arrays.toString(arraycopy));
+					
+					int num1 = arraycopy[temp[2]-1];
+					System.out.println((arraycopy[temp[2]-1]));
+					
+					answer[i] = num1;				
+					
+				}
+				
+				
+				
+				System.out.println(Arrays.toString(answer));
 
 	}
 
---
+++ CodingTest/src/ssh/sshTest01_01.java
@@ -0,0 +1,41 @@
+package ssh;
+
+import java.util.Arrays;
+
+public class sshTest01_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = {1,5,2,6,3,7,4};
+        int[][] commands = {{2,5,3},{4,4,1}, {1,7,3}};
+        int[] box = {};
+        int[] answer = new int[commands.length];
+
+
+        for (int i=0; i<commands.length; i++) {
+            box = Arrays.copyOfRange(array, commands[i][0]-1,
+                    commands[i][1]);    // {5,2,6,3}
+            
+            int temp;
+            int num1 = 0;
+            while(num1<=box.length) {
+            	for(int j=1;j<box.length;j++){
+                    if(box[j-1]>box[j]) {
+                        temp = box[j-1];
+                        box[j-1] = box[j];
+                        box[j] = temp;
+                    }
+            	}
+            	num1++;
+            }
+            
+            answer[i] = box[commands[i][2]-1];
+            for (int a : box) {
+                System.out.print(a+" ");
+            }
+            System.out.println(" ");
+        }
+
+	}
+
+}
